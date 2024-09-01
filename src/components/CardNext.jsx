f4d9d74e23efb3ec0a97509046a62b24479788a8
Ivan Mercedes <ivanmercede@gmail.com>
2024-09-01 10:21:25 -0400

    chore: remove useless lines

--- src/components/CardNext.jsx
+++ src/components/CardNext.jsx
@@ -5,10 +5,7 @@ export default function CardNext({ title, image, slug = null }) {
     <Card className="py-2">
       <CardBody className="overflow-visible py-2">
         {slug ? (
-          <a href={`${slug}`} 
-          ediaTransitionName={`media-image-${slug}`}
-          // style={{viewTransitionName:  `media-image-${slug?.replace("blog/", "")}`}}
-          >
+          <a href={`${slug}`} ediaTransitionName={`media-image-${slug}`}>
             <Image
               isBlurred
               alt={title}
@@ -26,8 +23,6 @@ export default function CardNext({ title, image, slug = null }) {
         )}
       </CardBody>
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        {/* <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small> */}
         <h4 className="font-bold text-large">{title}</h4>
       </CardHeader>
     </Card>
