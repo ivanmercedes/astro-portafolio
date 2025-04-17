b23796c5e982967ce118912007d0a6146c39e7b0
Ivan Mercedes <ivanmercede@gmail.com>
2025-04-17 14:57:57 -0400

    fix(Navbar): remove duplicate Link import and clean up Navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import {
+  Link,
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
 } from "@nextui-org/react";
 
 export default function App() {
@@ -16,11 +16,15 @@ export default function App() {
       </NavbarBrand>
       <NavbarContent className="sm:flex gap-4" justify="center">
         <NavbarItem>
+          <Link href="/projects" color="foreground" aria-current="page">
+            Poryectos
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
           <Link href="/blog" color="foreground" aria-current="page">
             Blog
           </Link>
         </NavbarItem>
-        <NavbarItem></NavbarItem>
       </NavbarContent>
     </Navbar>
   );
